815035f669f9a490a9450efb86db2f224091e4c8
Nazarii <stnaza8@gmail.com>
2018-09-24 00:23:50 +0300

    Lab3 done

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -4,36 +4,65 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
+    EditText fname, lname, phone, email, pass, cpass;
+    Button submit;
+    AwesomeValidation awesomeValidation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+        updateUI();
     }
+
+
 
     /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText3);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+    public void updateUI() {
+        fname =  findViewById(R.id.firstName);
+        lname =  findViewById(R.id.lastName);
+        phone =  findViewById(R.id.phone);
+        email =  findViewById(R.id.email);
+        pass =   findViewById(R.id.password);
+        cpass =  findViewById(R.id.confirmPassword);
+        submit = findViewById(R.id.submit);
 
-        // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView);
-        textView.setText("Hello  " + message);
+        String regexPassword = "(?=.*[a-z])(?=.*[A-Z])(?=.*[\\d])(?=.*[~`!@#\\$%\\^&\\*\\(\\)\\-_\\+=\\{\\}\\[\\]\\|\\;:\"<>,./\\?]).{8,}";
+        awesomeValidation.addValidation(MainActivity.this,R.id.firstName, "[a-zA-Z\\s]+", R.string.err_fname );
+        awesomeValidation.addValidation(MainActivity.this,R.id.lastName, "[a-zA-Z\\s]+", R.string.err_lname );
+        awesomeValidation.addValidation(MainActivity.this,R.id.email, android.util.Patterns.EMAIL_ADDRESS, R.string.err_email);
+        awesomeValidation.addValidation(MainActivity.this,R.id.phone, RegexTemplate.TELEPHONE, R.string.err_phone);
+        awesomeValidation.addValidation(MainActivity.this,R.id.password, regexPassword, R.string.err_pass );
+        awesomeValidation.addValidation(MainActivity.this,R.id.confirmPassword, R.id.password, R.string.err_cpass );
+
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (awesomeValidation.validate()) {
+                    Toast.makeText(MainActivity.this, "Succesfull", Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    public void clearMessage(View view) {
 
-        TextView textView = findViewById(R.id.editText3);
-        textView.setText("");
-        TextView textView1 = findViewById(R.id.textView);
-        textView1.setText("");
-    }
 }
