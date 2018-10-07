ea4e41bf1d9e6e6ea16522f4a67b8bccc2b3c173
Nazarii <stnaza8@gmail.com>
2018-10-07 17:49:00 +0300

    lab4 1.0

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,47 +1,41 @@
 package com.example.lab2;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.basgeekball.awesomevalidation.ValidationStyle;
-import com.basgeekball.awesomevalidation.utility.RegexTemplate;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-
-    EditText fname;
-    EditText lname;
-    EditText phone;
-    EditText email;
-    EditText pass;
-    EditText cpass;
-
-
-    Button submit;
-    AwesomeValidation awesomeValidation;
+    private EditText fname;
+    private EditText lname;
+    private EditText phone;
+    private EditText email;
+    private EditText pass;
+    private EditText cpass;
+    private int id = 1;
+    private Button submit, viewlist;
+    private AwesomeValidation awesomeValidation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
         updateUI();
     }
 
-
     /**
      * Called when the user taps the Send button
      */
-    public void updateUI() {
+    private void updateUI() {
         fname = findViewById(R.id.firstName);
         lname = findViewById(R.id.lastName);
         phone = findViewById(R.id.phone);
@@ -49,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         pass = findViewById(R.id.password);
         cpass = findViewById(R.id.confirmPassword);
         submit = findViewById(R.id.submit);
+        viewlist = findViewById(R.id.viewlist);
 
         String regexPhone = "[0-9]{10}";
         String regexPassword = "(?=.*[a-z])(?=.*[A-Z])(?=.*[\\d])(?=.*[~`!@#\\$%\\^&\\*\\(\\)\\-_\\+=\\{\\}\\[\\]\\|\\;:\"<>,./\\?]).{8,}";
@@ -59,18 +54,34 @@ public class MainActivity extends AppCompatActivity {
         awesomeValidation.addValidation(MainActivity.this, R.id.password, regexPassword, R.string.err_pass);
         awesomeValidation.addValidation(MainActivity.this, R.id.confirmPassword, R.id.password, R.string.err_cpass);
 
-
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (awesomeValidation.validate()) {
-                    Toast.makeText(MainActivity.this, "Succesfull", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                    saveToPreferences();
                 } else {
                     Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+        viewlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent activityChangeIntent = new Intent(MainActivity.this, UserDataActivity.class);
+                MainActivity.this.startActivity(activityChangeIntent);
+            }
+        });
     }
 
-
+    private void saveToPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences("userS", Context.MODE_PRIVATE);
+        id = sharedPreferences.getAll().size()/4 + 1;
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("name" + id, fname.getText().toString());
+        editor.putString("surname" + id, lname.getText().toString());
+        editor.putString("email" + id, email.getText().toString());
+        editor.putString("phone" + id, phone.getText().toString());
+        editor.apply();
+    }
 }
